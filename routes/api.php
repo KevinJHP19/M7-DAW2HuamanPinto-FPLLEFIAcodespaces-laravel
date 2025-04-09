086f627fa4c114808ff00c53aa7022fdb16c2cec
KevinJHP19 <huamanpintokevin@fpllefia.com>
2025-04-09 14:36:34 +0000

    crear rutas de api y crear tabla users con migracion

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/users', function (){
+    return 'Lista de usuarios';
+});
+Route::post('/users', function (){
+    return 'Crear usuario';
+});
+Route::put('/users/{id}', function (){
+    return 'Actualizar usuario';
+});
+Route::delete('/users/{id}', function (){
+    return 'Eliminar usuario';
+});
+Route::get('/users/{id}', function (){
+    return 'Obtener un simple usuario';
+});
+
+
+
